5061692445e95a04844d6b0708de4cbeb2691746
shiratama-y0822 <shiratama.y0822@gmail.com>
2026-08-27 23:50:54 +0900

    HandsOn実施

--- Hands_on/javascript-practice/practice.js
+++ Hands_on/javascript-practice/practice.js
@@ -1,5 +1,20 @@
-const pokemonType = "water";
+const pokemon = [
+    {
+        name: "Pikachu",
+        level: 25,
+    },
+    {
+        name: "Charmander",
+        level: 18,
+    },
+    {
+        name: "Squirtle",
+        level: 20,
+    },
+];
 
-if (pokemonType === "Electric") {
-    console.log("でんきタイプです");
+for (let i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].level >= 20) {
+        console.log(pokemon[i].name);
+    }
 }
