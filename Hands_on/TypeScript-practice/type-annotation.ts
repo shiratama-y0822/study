235255c691806253393e8575b2117610622e1301
shiratama-y0822 <shiratama.y0822@gmail.com>
2026-09-01 17:40:00 +0900

    単語学習（generic概要）、Hands on（Type Annotation）

---
+++ Hands_on/TypeScript-practice/type-annotation.ts
@@ -0,0 +1,14 @@
+let pokemonName: string = "Pikachu";
+console.log(pokemonName);
+
+let pokemonLevel: number = 25;
+console.log(pokemonLevel);
+
+let isFavorite: boolean = true;
+console.log(isFavorite);
+
+let pokemonType = "Electric";
+pokemonType = 100;
+
+let pokemonLevel2 = 25;
+pokemonLevel2 = "Level 30";
